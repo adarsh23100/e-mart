3e95cbf9edc181320984de7593d28fa8692baf1e
Adarsh23100 <adarshrajesh23100@gmail.com>
2022-04-06 23:04:26 +0530

    New commit

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,6 +1,7 @@
 const Product = require("../models/productModel");
 const ErrorHandler = require("../utils/errorHandler");
-const catchAsyncErrors = require("../middleware/catchAsyncErrors")
+const catchAsyncErrors = require("../middleware/catchAsyncErrors");
+const Apifeatures = require("../utils/apifeatures");
 
 // Create Product -- Admin
 exports.createProduct = catchAsyncErrors(async (req,res,next)=>{
@@ -14,7 +15,12 @@ exports.createProduct = catchAsyncErrors(async (req,res,next)=>{
 
 // Get All Products
 exports.getAllProducts = catchAsyncErrors(async (req,res) => {
-    const products = await Product.find();
+
+    const resultPerPage = 5;
+    const productCount = await Product .countDocuments();
+
+    const apiFeature = new Apifeatures(Product.find(),req.query).search().filter().pagination(resultPerPage);
+    const products = await apiFeature.query;
     res.status(200).json({
         success:true,
         products
@@ -31,9 +37,10 @@ exports.getProductDetails = catchAsyncErrors(async (req,res,next)=>{
 
     res.status(200).json({
         success:true,
-        product
+        product,
+        productCount
     });
-})
+});
 
 // Update Product -- Admin
 
