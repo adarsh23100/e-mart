a7d88b8cf08a57dd25f7a6efea126211b7ff03a3
Adarsh23100 <adarshrajesh23100@gmail.com>
2022-04-08 00:35:44 +0530

    New commit

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -4,5 +4,16 @@ const User = require("../models/userModel");
 
 // Register a user
 exports.registerUser = catchAsyncErrors( async(req,res,next)=>{
-    
+    const {name,email,password} = req.body;
+    const user = await User.create({
+        name,email,password,
+        avatar:{
+            public_id:"This a sample id",
+            url:"profileUrl"
+        }
+    });
+    res.status(201).json({
+        success:true,
+        user
+    })
 })
