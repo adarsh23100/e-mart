75b21838d5d6ccb59eb3a367bc1439105c25007d
Adarsh23100 <adarshrajesh23100@gmail.com>
2022-04-12 23:53:29 +0530

    New commit

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,6 +1,7 @@
 const ErrorHandler = require("../utils/errorHandler");
 const catchAsyncErrors = require("../middleware/catchAsyncErrors");
 const User = require("../models/userModel");
+const sendToken = require("../utils/jwtToken");
 
 // Register a user
 exports.registerUser = catchAsyncErrors( async(req,res,next)=>{
@@ -12,8 +13,41 @@ exports.registerUser = catchAsyncErrors( async(req,res,next)=>{
             url:"profileUrl"
         }
     });
-    res.status(201).json({
-        success:true,
-        user
-    })
-})
+
+    sendToken(user,201,res);
+});
+
+// Login User
+exports.loginUser = catchAsyncErrors(async (req,res,next)=>{
+    const {email,password} = req.body;
+
+    // checking if user has given password and email both
+    if(!email || !password){
+        return next(new ErrorHandler("Please enter email and password",400));
+    }
+
+    const user = await User.findOne({email}).select("+password");
+    if(!user){
+        return next(new ErrorHandler("Invalid email or password",401));
+    }
+
+    const isPasswordMatched = user.comparePassword(password);
+    if(!isPasswordMatched){
+        return next(new ErrorHandler("Invalid email or password",401));
+    }
+
+    sendToken(user,200,res);
+});
+
+// Logout user
+exports.logout = catchAsyncErrors(async(req,res,next)=>{
+    res.cookie("token",null,{
+        expires: new Date(Date.now()),
+        httpOnly: true
+    });
+
+    res.status(200).json({
+        success: true,
+        message: "Logged Out"
+    });
+});
