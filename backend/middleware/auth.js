8238fa7588e1372f414b491883a188d5a1a0a231
Adarsh23100 <adarshrajesh23100@gmail.com>
2022-04-13 00:41:17 +0530

    New commit

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -12,8 +12,15 @@ const User = require("../models/userModel")
     }
 
     const decodedData = jwt.verify(token, process.env.JWT_SECRET);
-
     req.user = await User.findById(decodedData.id);
-
     next();
- })
+ });
+
+ exports.authorizedRoles = (...roles)=>{
+    return (req,res,next)=>{
+        if(!roles.includes(req.user.role)){
+            return next( new ErrorHandler(`Role: ${req.user.role} is not allowed to access this resource`,403) );
+        }
+        next();
+    };
+ };
