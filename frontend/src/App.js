f98f226a3c15acf26694a6c0ec8e195d9e0d42e0
Adarsh23100 <adarshrajesh23100@gmail.com>
2022-04-25 19:38:58 +0530

    Header and Footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
-import Header from "./component/layout/Header"
+import Header from "./component/layout/Header/Header"
+import Footer from "./component/layout/Footer/Footer"
 import {BrowserRouter as Router} from "react-router-dom"
 import WebFont from "webfontloader"
 import React from 'react';
 
-React.useEffect(() => {
+function App() {
+
+  React.useEffect(() => {
   WebFont.load({
     google:{
       families:["Roboto","Droid Sans","Chilanka"]
@@ -12,10 +15,10 @@ React.useEffect(() => {
   })
 },[])
 
-function App() {
   return (
     <Router> 
       <Header />
+      <Footer />
     </Router>
   );
 }
